69d74ad635c0b018bcb4bcc210f240c58adcfd0a
Saeed Sabra <saeedsabra565@gmail.com>
2023-07-24 23:43:19 +0300

    finish the Header With Search Box

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/home/components/header_with_search_box.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -8,93 +9,10 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        SizedBox(
-          height: size.height * 0.25,
-          child: Stack(
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                height: size.height * 0.25 - 32,
-                decoration: const BoxDecoration(
-                  color: kPrimaryColor,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36),
-                  ),
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      "Hi Uishopy!",
-                      style:
-                          Theme.of(context).textTheme.headlineMedium?.copyWith(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                      // TextStyle(
-                      //   fontSize: 36,
-                      //   color: Colors.white,
-                      //   fontWeight: FontWeight.bold,
-                      // ),
-                    ),
-                    const Spacer(),
-                    Image.asset(
-                      "assets/images/logo.png",
-                    )
-                  ],
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  alignment: Alignment.center,
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  height: 54,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.23),
-                      )
-                    ],
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          onChanged: (value) {},
-                          decoration: InputDecoration(
-                            hintText: "Search",
-                            hintStyle: TextStyle(
-                              color: kPrimaryColor.withOpacity(0.5),
-                            ),
-                            enabledBorder: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            // suffixIcon: SvgPicture.asset("assets/icons/search.svg"),
-                          ),
-                        ),
-                      ),
-                      SvgPicture.asset(
-                        "assets/icons/search.svg",
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        )
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [HeaderWithSearchBox(size: size)],
+      ),
     );
   }
 }
